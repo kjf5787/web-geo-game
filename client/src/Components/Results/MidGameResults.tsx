52b9f9b8124855d5b9cb9565b4a6095f4b34999f
Kayla Fennell <kjf5787@rit.edu>
2026-06-19 12:52:16 +0100

    Add confetti for top player

--- client/src/Components/Results/MidGameResults.tsx
+++ client/src/Components/Results/MidGameResults.tsx
@@ -1,23 +1,45 @@
-// Authors: Vojta Bruza and Grace Houser
+// Authors: Vojta Bruza, Grace Houser, and Kayla Fennell
 // Results Page 
 
 import { Box, Button, Center, Text } from "@chakra-ui/react";
-import { useMemo } from "react";
+import { useMemo, useEffect, useState } from "react";
 import { useConnection } from "../Contexts/ConnectionContext";
 import { useGameRoom } from "../Contexts/GameRoomContext";
 import PlayerRanking from "./PlayerRanking";
 import { useTranslation } from "react-i18next";
-
+import { useRankedPlayers } from "./useRankedPlayers";
+import Confetti from 'react-confetti'
 
 export default function MidGameResults() {
     const { t } = useTranslation();
     const { isFacilitator } = useGameRoom();
     const { socket, localPlayerID } = useConnection();
+    const rankedPlayers = useRankedPlayers();
+    const isTopOfList = useMemo(
+        () => rankedPlayers[0]?.id === localPlayerID,
+        [rankedPlayers, localPlayerID]
+    );
+    const [showConfetti, setShowConfetti] = useState(false)
 
     const isFac = useMemo(() => isFacilitator(localPlayerID), [localPlayerID, isFacilitator]);
 
+    useEffect(() => {
+        if (isTopOfList) {
+            setShowConfetti(true)
+        }
+    }, [isTopOfList]);
+
     return (
         <Box overflow="auto" h="100%">
+            {showConfetti && (
+                <Confetti
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    recycle={false}
+                    gravity={0.15}
+                    onConfettiComplete={() => setShowConfetti(false)}
+                />
+            )}
 
             {/* Page Title */}
             <Center>
