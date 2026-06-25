131cee0988506c0149e7987990e2a32ded20ed3e
Kayla Fennell <kjf5787@rit.edu>
2026-06-25 15:45:09 +0100

    Add mui icon to avatar

--- client/src/Components/Lobby/Icon.tsx
+++ client/src/Components/Lobby/Icon.tsx
@@ -1,12 +1,14 @@
-// Authors: Vojtech Bruza and Grace Houser
+// Authors: Vojtech Bruza, Grace Houser, and Kayla Fennell
 import { Avatar, Box } from "@chakra-ui/react";
 import React from "react";
 import "../../Theme/theme.css";
 import { global_icon_colors } from "../../data/DataTypes";
+import * as MuiIcons from "@mui/icons-material";
 
 // IconProps interface
 interface IconProps {
     color: string;
+    icon?: string;
 }
 
 export const getIconColor = (color: string | undefined, alpha = 1) => {
@@ -28,15 +30,27 @@ const hexToRgba = (hex: string, alpha: number) => {
     return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 };
 
-const Icon: React.FC<IconProps> = ({ color }) => {
-
+const Icon: React.FC<IconProps> = ({ color, icon }) => {
+    
     // Determine the CSS variable for the user's selected color
     const userColor = getIconColor(color);
+    const MuiIcon = icon ? (MuiIcons as any)[icon] : MuiIcons.Person;
 
     // Return the Icon with the appropriate background color
     return (
-        <Box alignSelf="center" ml="10px" mr="21px">
-            <Avatar display="inline-block" bg={userColor} />
+        <Box
+            alignSelf="center"
+            ml="10px"
+            mr="21px"
+            bg={userColor}
+            borderRadius="full"
+            width="40px"
+            height="40px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <MuiIcon sx={{ color: "white", fontSize: 24 }} />
         </Box>
     );
 };
